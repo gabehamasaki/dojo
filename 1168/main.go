ba041b4059f12e57762a8a26d961d91e0ee94b41
Gabriel Hamasaki <gabriel.hamasaki@exata.it>
2024-10-03 21:10:19 -0300

    resolve 1168 & 2164

--- 1168/main.go
+++ 1168/main.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 // https://judge.beecrowd.com/pt/problems/view/1168
 func main() {
+	lines := 0
+
+	leds := map[int]int{
+		1: 2,
+		2: 5,
+		3: 5,
+		4: 4,
+		5: 5,
+		6: 6,
+		7: 3,
+		8: 7,
+		9: 6,
+		0: 6,
+	}
+
+	fmt.Scanln(&lines)
+
+	for i := 0; i < lines; i++ {
+		var n string
+		fmt.Scanln(&n)
+
+		total := 0
+		for _, r := range n {
+			v := string(r)
+			index, err := strconv.Atoi(v)
+			if err != nil {
+				panic(err)
+			}
+
+			total += leds[index]
+		}
+		fmt.Println(total, "leds")
+	}
 }
--- 2164/main.go
+++ 2164/main.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 // https://judge.beecrowd.com/pt/problems/view/2164
 func main() {
+	input := 0.0
+	fmt.Scan(&input)
 
+	result := fast(input)
+
+	fmt.Printf("%.1f\n", result)
+}
+
+func fast(n float64) float64 {
+	output := (math.Pow((1+math.Sqrt(5))/2, n) - math.Pow((1-math.Sqrt(5))/2, n)) / math.Sqrt(5)
+
+	return output
 }
